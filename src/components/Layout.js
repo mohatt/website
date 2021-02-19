bf7312f4c0196465f76005411426ebe0025d6c74
Mohamed Elkholy <mkh117@gmail.com>
2021-02-19 06:28:50 +0200

    perf(site): preload avatar images using http headers instead of html head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,8 +24,6 @@ const Layout = ({ children, title = null, description = null }) => {
         <meta name='twitter:title' content={title} />
         <meta name='twitter:card' content='summary' />
         <meta name='twitter:description' content={description} />
-        <link as='image' rel='preload' href={require('../assets/img/avatar/avatar.png')} />
-        <link as='image' rel='preload' href={require('../assets/img/avatar/avatar-smile.png')} />
         <body className='font-sans antialiased leading-normal text-base text-typo' />
       </Helmet>
       <Header />
