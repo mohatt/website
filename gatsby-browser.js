675512c620711fd46c4c4982dd6d6bc2fb78cb7f
Mohamed Elkholy <mkh117@gmail.com>
2021-04-11 10:00:33 +0200

    feat(site): add useLoading hook

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,22 @@
 export { default as wrapRootElement } from './src/wrapRootElement'
+
+/**
+ * Emitts an event with loading state
+ *
+ * @see ./src/hook/loading
+ */
+function setLoadingState(state) {
+  window.___emitter.emit('___loading', state)
+}
+
+export function onPreRouteUpdate() {
+  setLoadingState('started')
+}
+
+export function onRouteUpdate() {
+  setLoadingState('done')
+}
+
+export function onRouteUpdateDelayed() {
+  setLoadingState('delayed')
+}
