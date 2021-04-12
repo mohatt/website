97b4b4fd9a08fd95f6777214271efd21d3a7ef30
Mohamed Elkholy <mkh117@gmail.com>
2021-04-12 05:23:53 +0200

    perf(site): removed unused import

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
-import './Spinner.css'
 
-export default function Spinner({ color, className }) {
+export default function Spinner({ className }) {
   return (
   <svg className={classNames('spinner', className)} viewBox="0 0 50 50">
     <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5" />
