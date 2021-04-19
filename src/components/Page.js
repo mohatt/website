1ea625666bbec1dfe982c9fa4c4bd0fe639a5855
Mohamed Elkholy <mkh117@gmail.com>
2021-04-19 23:04:22 +0200

    perf(site): split page helmet and snippet into two components

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,62 +3,91 @@ import Helmet from 'react-helmet'
 import { useSiteMetadata } from '../hooks'
 import { Layout, Section } from '.'
 
-const PageMeta = React.memo(({ title, description, pretext }) => {
+function PageHelmet({ title, description }) {
   const site = useSiteMetadata()
   const seoTitle = title ? `${title} — ${site.title}` : site.title
   const seoDescription = description || site.description
-  if (typeof pretext !== 'string') {
-    const func = pretext?.func || (title || site.title).replace(/[-_\s.]+(.)?/g, (_, c) => c ? c.toUpperCase() : '')
-    const args = pretext?.args || {}
-    pretext = `render(<${func} ${Object.keys(args)
-      .filter(a => args[a] !== undefined)
-      .map(a => `${a}=${JSON.stringify(args[a])} `)
+  return (
+    <Helmet>
+      <title>{seoTitle}</title>
+      <meta name='description' content={seoDescription} />
+      <meta name='og:title' content={seoTitle} />
+      <meta name='og:description' content={seoDescription} />
+      <meta name='og:type' content='website' />
+      <meta name='og:site_name' content={site.title} />
+      <meta name='twitter:title' content={seoTitle} />
+      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:description' content={seoDescription} />
+    </Helmet>
+  )
+}
+
+function PageSnippet({ $title, $text, $type, ...props }) {
+  if (!$text) {
+    $type = $type || $title.replace(/[-_\s.]+(.)?/g, (_, c) => c ? c.toUpperCase() : '')
+    $text = `render(<${$type} ${Object.keys(props)
+      .filter(a => props[a] !== undefined)
+      .map(a => `${a}=${JSON.stringify(props[a])} `)
       .join('')
     }/>);`
   }
   return (
-    <>
-      <Helmet>
-        <title>{seoTitle}</title>
-        <meta name='description' content={seoDescription} />
-        <meta name='og:title' content={seoTitle} />
-        <meta name='og:description' content={seoDescription} />
-        <meta name='og:type' content='website' />
-        <meta name='og:site_name' content={site.title} />
-        <meta name='twitter:title' content={seoTitle} />
-        <meta name='twitter:card' content='summary' />
-        <meta name='twitter:description' content={seoDescription} />
-      </Helmet>
-      <Section spacing={false} className='font-display italic'>{pretext}</Section>
-    </>
+    <Section spacing={false} className='font-display italic'>{$text}</Section>
   )
-})
+}
 
+/**
+ * Base component for all page components
+ * @abstract
+ */
 export default class Page extends React.Component {
   /**
-   * Page metadta
+   * Page seo title
+   * @type string
    */
-  title = null
-  description = null
-  pretext = null
+  title
+
+  /**
+   * Page seo description
+   * @type string
+   */
+  description
+
+  /**
+   * Page snippet
+   * @type Object|string
+   */
+  snippet = {}
 
   /**
    * Default layout
+   * @type Function
    */
   static layout = Layout
 
   /**
-   * Should be implemented by page components
+   * Should be implemented by child Page components
+   * @return {JSX.Element}
    */
   view() {
     return null
   }
 
+  /**
+   * Renders Page view template
+   * @return {JSX.Element}
+   */
   render() {
     const content = this.view()
+    if (typeof this.snippet === 'string') {
+      this.snippet = {
+        $text: this.snippet
+      }
+    }
     return (
       <>
-        <PageMeta title={this.title} description={this.description} pretext={this.pretext} />
+        <PageHelmet title={this.title} description={this.description} />
+        <PageSnippet $title={this.title} {...this.snippet} />
         {content}
       </>
     )
