febe98a0c76e6c2122a4d2c9d6e81c24d63030a8
Mohamed Elkholy <mkh117@gmail.com>
2021-04-23 09:37:55 +0200

    fix(scroll): use position: sticky to avoid Gatsby's scrolling issues
    SEE THESE ISSUES
    https://github.com/gatsbyjs/gatsby/issues/31012
    https://github.com/w3c/csswg-drafts/issues/865

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,25 +12,25 @@ export default function Layout({ children }) {
   const { path } = useCurrentPath()
   const isHome = path === '/'
   return (
-    <div className='h-screen flex flex-row overflow-hidden'>
+    <div className='flex flex-row'>
       <LayoutContext.Provider value={{ menuOpen, setMenuOpen }}>
         <div
           id='header'
-          className='w-24 flex-shrink-0 border-r-4 border-primary bg-typo text-primary z-20'>
+          className='w-24 h-screen sticky top-0 flex-shrink-0 border-r-4 border-primary bg-typo text-primary z-20'>
           <Header className='h-full relative' />
         </div>
         <div
           id='menu'
           className={`${
             menuOpen ? 'w-64' : 'w-0'
-          } lg:w-64 overflow-hidden flex-shrink-0 flex flex-col justify-center bg-accent text-typo-dim text-shadow transition-box`}>
+          } lg:w-64 h-screen sticky top-0 overflow-hidden flex-shrink-0 flex flex-col justify-center bg-accent text-typo-dim text-shadow transition-box`}>
           <Menu isHome={isHome} className='w-64' />
         </div>
         <div
           id='main'
           className={`${
             menuOpen ? '-mr-64' : ''
-          } lg:-mr-0 flex-grow overflow-y-auto flex flex-col bg-secondary text-typo-dim text-shadow transition-box`}>
+          } lg:-mr-0 flex-grow flex flex-col bg-secondary text-typo-dim text-shadow transition-box`}>
           <main className='mt-32 flex-grow flex-shrink-0 flex flex-col justify-center'>
             {children}
           </main>
