6007ff9967d1931dd2fcacabd580f0432ca8237b
Mohamed E <mkh117@gmail.com>
2021-04-30 04:13:48 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,12 +61,7 @@ module.exports = {
       resolve: 'gatsby-plugin-postcss',
       options: require('./config/postcss'),
     },
-    {
-      resolve: 'gatsby-plugin-vercel',
-      options: {
-        debug: true
-      },
-    },
+    'gatsby-plugin-vercel',
     'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
     'gatsby-plugin-catch-links',
