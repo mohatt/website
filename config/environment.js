251d2d93ebaf61c9b1e7e6daa3bbc5f03fd82366
Mohamed Elkholy <mkh117@gmail.com>
2021-05-04 02:15:49 +0200

    feat(site): update config for staging and production environments

--- config/environment.js
+++ config/environment.js
@@ -1,15 +1,15 @@
 const firebaseConfig = {
   dev: {
-    apiKey: "AIzaSyCCUU0dXV_m5CIP2WgO8iOlBhQKVK0gkk4",
-    projectId: "mohatt",
-    appId: "1:485833880063:web:3106eba17514f9d5b377ca",
-    measurementId: "G-K3VND9PPEE"
+    apiKey: "AIzaSyDt0r8Z9pEvLYJBSa4YtsoApnFKCZrOvCs",
+    projectId: "mohatt-staging",
+    appId: "1:809896687731:web:1e7d87d140172f0c1bd206",
+    measurementId: "G-R27D7B0GT7"
   },
   prod: {
     apiKey: "AIzaSyCCUU0dXV_m5CIP2WgO8iOlBhQKVK0gkk4",
     projectId: "mohatt",
     appId: "1:485833880063:web:3106eba17514f9d5b377ca",
-    measurementId: "G-K3VND9PPEE"
+    measurementId: "G-LTLQD2WEPN"
   }
 }
 
