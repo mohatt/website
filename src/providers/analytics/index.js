2662e061f15f1d14cfbc7f0995655c4a28749edb
Mohamed Elkholy <mkh117@gmail.com>
2021-05-13 16:14:46 +0200

    perf(site): no need for `AnalyticsContext`

--- src/providers/analytics/index.js
+++ src/providers/analytics/index.js
@@ -1,42 +1,38 @@
-import React, { useContext, useEffect, useRef, useMemo } from 'react'
+import { useEffect, useRef, useMemo } from 'react'
 import { environmentConfig, isBrowser } from '../../commons/environment'
 import { useCurrentLocation } from '../../hooks'
 import { Analytics, initializeAnalytics } from './analytics'
 
-const AnalyticsContext = React.createContext()
 const analytics = new Analytics(environmentConfig.analyticsId, {
   send_page_view: false
 })
 
 export function AnalyticsProvider({ children }) {
   const location = useCurrentLocation()
-  const referrer = useRef()
+  const url = location.href
+  const referrer = useRef(isBrowser && document.referrer)
 
-  useMemo(() => {
-    if (!isBrowser) {
-      return
-    }
-    analytics.config({
+  useAnalyticsCallback(({ config }) => {
+    config({
       page_path: location.pathname + location.search + location.hash,
-      page_referrer: referrer.current || document.referrer
+      page_referrer: referrer.current
     })
-    referrer.current = location.href
-  }, [location.href])
+    referrer.current = url
+  }, [url])
 
   useEffect(() => initializeAnalytics(analytics), [])
 
-  return (
-    <AnalyticsContext.Provider value={analytics}>
-      {children}
-    </AnalyticsContext.Provider>
-  )
+  return children
 }
 
 export function useAnalytics() {
-  return useContext(AnalyticsContext)
+  return analytics
 }
 
 export function useAnalyticsCallback(callback, deps) {
-  const analytics = useAnalytics()
-  return useMemo(() => isBrowser && callback(analytics), deps)
+  return useMemo(
+    () => isBrowser && callback(analytics),
+    // eslint-disable-next-line
+    deps
+  )
 }
