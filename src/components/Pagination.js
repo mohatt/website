472a55653942a07e5154f33fba751de18690ad67
Mohamed Elkholy <mkh117@gmail.com>
2021-06-29 17:19:31 +0200

    fix(site): css tweaks

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,9 +2,20 @@ import React from 'react'
 import { Pagination as PaginationPlugin } from 'gatsby-plugin-advanced-pages'
 
 export default function Pagination(props) {
+  if (!props.pageInfo.pageCount) {
+    return null
+  }
+
   return (
-    <>
-      <PaginationPlugin {...props} />
-    </>
+    <PaginationPlugin
+      className='mt-12'
+      ui='mini'
+      theme={{
+        item: 'btn btn-alt rounded-full',
+        'item.next': 'float-right',
+      }}
+      renderDisabled={false}
+      {...props}
+    />
   )
 }
