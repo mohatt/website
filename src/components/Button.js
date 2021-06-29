eb7c4e747563691937888ed58fad6bd020b32a1b
Mohamed Elkholy <mkh117@gmail.com>
2021-06-29 20:11:28 +0200

    fix(site): css tweaks to Button components

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,20 @@ import React from 'react'
 import classNames from 'classnames'
 import { Link } from '.'
 
-export default function Button({ color, active, mono, rounded = true, outline, className, ...props }) {
+export default function Button({ color, size, mono, rounded = true, active, disabled, outline, className, ...props }) {
   const Element = props.to ? Link : 'button'
   return (
     <Element
       className={classNames(
         'btn',
         color && `btn-${color}`,
+        size && `btn-${size}`,
         {
           'rounded-full': rounded,
           'p-2': mono,
           'btn-outline': outline,
           'btn-active': active,
+          'btn-disabled': disabled,
         },
         className
       )}
