9adf515adba087f84b55b778268d8d2cafa92c78
Mohamed Elkholy <mkh117@gmail.com>
2025-09-06 02:08:05 +0400

    fix(site): fix print layout sidebar, remove profile photo
    Signed-off-by: Mohamed Elkholy <mkh117@gmail.com>

--- src/layouts/print/index.js
+++ src/layouts/print/index.js
@@ -4,15 +4,12 @@ import { DocumentHead } from '../partials'
 
 export default function PrintLayout({ layout: { setPrintLayout, isEnforced }, children }) {
   return (
-    <div className='flex max-w-[8.5in] mx-auto shadow-2xl print:shadow-none'>
-      <header id='header' className='relative w-12 flex-shrink-0 border-r-2 bg-typo text-primary'>
+    <div className='max-w-[8.5in] mx-auto shadow-2xl print:shadow-none'>
+      <div id='header' className='fixed top-0 bottom-0 w-12 border-r-2 bg-typo text-primary'>
         <DocumentHead />
-        <div className='absolute w-16 top-0 right-0 mt-6 -mr-8 text-center'>
-          <ul>
-            <li className='mb-6'>
-              <a id='avatar' className='block rounded-full h-16 bg-accent bg-cover bg-center' />
-            </li>
-            {isEnforced && (
+        {isEnforced && (
+          <div className='absolute w-16 top-0 right-0 mt-6 -mr-8 text-center'>
+            <ul>
               <li className='mb-4 print:hidden'>
                 <Button
                   size='mono'
@@ -22,13 +19,13 @@ export default function PrintLayout({ layout: { setPrintLayout, isEnforced }, ch
                   <Icon name='back' className='w-5' />
                 </Button>
               </li>
-            )}
-          </ul>
-        </div>
-      </header>
+            </ul>
+          </div>
+        )}
+      </div>
       <main
         id='main'
-        className='pt-6 flex-grow flex flex-col justify-center bg-secondary text-typo-dim text-shadow'
+        className='ml-12 flex flex-col justify-center bg-secondary text-typo text-shadow'
       >
         {children}
       </main>
