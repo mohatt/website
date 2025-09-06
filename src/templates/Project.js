c1c2208e268ff8c1d6904c8059b6cc3675789593
Mohamed Elkholy <mkh117@gmail.com>
2025-09-06 05:51:48 +0400

    feat(site): update content for ope-source projects
    Signed-off-by: Mohamed Elkholy <mkh117@gmail.com>

--- src/templates/Project.js
+++ src/templates/Project.js
@@ -105,8 +105,8 @@ export default class Project extends Page {
             <NetworkHandle.Map data={project.handles}>
               {(items) => <Metadata title='Links'>{items}</Metadata>}
               {({ title, href, Icon }) => (
-                <Link to={href} external='project_link' className='link mr-4'>
-                  <Icon className='w-5 mr-1' />
+                <Link to={href} external='project_link' className='link mr-4 mb-1'>
+                  <Icon className='w-5 mr-2' />
                   <span>{title}</span>
                 </Link>
               )}
